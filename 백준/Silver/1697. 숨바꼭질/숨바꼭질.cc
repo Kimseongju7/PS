7e28b70e6abc4808984c13a6579a9c1820cd758e
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2024-06-25 23:20:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 40 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<queue>
+#include<utility>
+using namespace std;
+#define endl '\n'
+#define N 100000
+#define INF 1e9
+vector<pair<int, int> > G[202020];
+int s, k;
+int D[202020], C[202020];
+
+void Dijkstra(){
+    for(int i = 0; i < 202020; ++i) D[i] = INF, C[i] = 0;
+    D[s] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<> > pq;
+    pq.emplace(D[s], s);
+    while(!pq.empty()){
+        auto [c, v] = pq.top(); pq.pop();
+        if(C[v]) continue;
+        C[v] = 1;
+        for(auto [i, w] : G[v]){
+            if(D[i] > D[v] + w){
+                D[i] = D[v] + w;
+                pq.emplace(D[i], i);
+            }
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    cin >> s >> k;
+    G[0].emplace_back(1, 1);
+    for(int i = 1; i <= N; ++i){
+        G[i].emplace_back(i - 1, 1);
+        G[i].emplace_back(i + 1, 1);
+        G[i].emplace_back(2 * i, 1);
+    }
+    Dijkstra();
+    cout << D[k];
+    return 0;
+}
