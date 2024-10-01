c630db60c2eeda9d0becabdfcd4e01a23b2bc8ac
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2024-10-01 22:19:12 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 0 ms, Memory: 2060 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.cc
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.cc
@@ -1,31 +1,52 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 #define INF 1e9
-
-int D[101][101], N, M;
-int dap1, dap2, dapSum = INF;
-
+using namespace std;
+ 
+ 
+int N, M;
+int d[101][101];
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    cin >> N >> M;
-    for(int i = 1 ;i <= N; ++i) for(int j = 1; j <= N; ++j) D[i][j] = 1;
-    for(int i = 1; i <= N; ++i) D[i][i] = 0;
-    for(int i = 1;i <= M; ++i){
-        int u, v; cin >> u >> v;
-        D[u][v] = 1; D[v][u] = 1;
-    }
-    for(int k = 1; k <= N; ++k) for(int i = 1; i <= N; ++i) for(int j = 1; j <= N; ++j) D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
-    for(int i = 1; i <= N; ++i) for(int j = 1; j <= N; ++j){
-        int sum = 0;
-        for(int k = 1; k <= N; ++k){
-            sum += min(D[i][k], D[j][k]);
-        }
-        if(sum < dapSum){
-            dapSum = sum;
-            dap1 = i;
-            dap2 = j;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    for(int i=0; i<101; i++){
+        for(int k=0; k<101; k++){
+            if(i==k) d[i][k] = 0;
+            else d[i][k] = INF;
         }
     }
-    cout << dap1 << ' ' << dap2 << ' ' << dapSum * 2;
-    return 0;
+    cin>>N>>M;
+    
+    for(int i=0; i<M; i++){
+        int a, b; cin>>a>>b;
+        d[a][b] = 1;
+        d[b][a] = 1;
+    }
+    
+    for(int k=1; k<=N; k++){
+        for(int a=1; a<=N; a++){
+            for(int b=1; b<=N; b++){
+                d[a][b] = min(d[a][b], d[a][k] + d[k][b]);
+            }
+        }
+    }
+    
+    int chick1 = 0, chick2 = 0;
+    int min_d = 1e9;
+    
+    for(int i=1; i<=N; i++){    // 치킨집 고르기
+        for(int j=i+1; j<=N; j++){
+            int temp_d = 0;
+            for(int k = 1; k<=N; k++){
+                temp_d += min(i==k?0:d[i][k], j==k?0:d[j][k]);    // 치킨집까지의 거리 갱신
+            }
+            if(temp_d < min_d){
+                min_d = temp_d;
+                chick1 = i;
+                chick2 = j;
+            }
+        }
+    }
+    
+    cout<< chick1 << ' ' << chick2 << ' ' << min_d * 2;
 }
