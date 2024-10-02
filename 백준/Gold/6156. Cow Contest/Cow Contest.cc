3b2125862a622bc12ea4603f1e8bae706646ad57
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2024-10-02 23:34:19 +0900

    [Gold IV] Title: Cow Contest, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/6156. Cow Contest/Cow Contest.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+using  namespace std;
+#define INF 1e9
+int N, M, D[101][101];
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); 
+    cin >> N >> M;
+    for(int i = 1; i <= N; ++i) for(int j = 1; j <= N; ++j) D[i][j] = INF;
+    for(int i = 1; i <= N; ++i) D[i][i] = 0;
+    for(int i = 1; i <= M; ++i){
+        int s, e; cin >> s >> e;
+        D[s][e] = 1;
+    }
+    for(int k = 1; k <= N; ++k) for(int i = 1; i <= N; ++i) for(int j = 1;  j <= N; ++j) {
+        D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
+    }
+    int dap = 0;
+    for(int i = 1; i <= N; ++i){
+        int sum = 0;
+        for(int j= 1; j <= N; ++j){
+            if(D[i][j] != INF || D[j][i] != INF) ++sum;
+        }
+        if(sum == N) ++dap;
+    }
+    cout << dap;
+    return 0;
+}
