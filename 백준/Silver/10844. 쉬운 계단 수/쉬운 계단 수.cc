7d3bdbfdb24ea0416559c1239c68aafae9b7e78b
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2024-11-17 19:18:09 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -10,11 +10,11 @@ int main(){
     for(int i = 2; i <= N; ++i){
         for(int j = 0; j <= 9; ++j){
             if(j == 0) dp[i][j] = dp[i - 1][j + 1];
-            else if(j == 9) dp[i][j] = dp[i - 1][j - 1];
+            else if(j == 9) dp[i][j] = dp[i - 1][j - 1] ;
             else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % M;
         }
     }
-    for(int i = 0; i <= 9; ++i) dap = (dap + dp[N][i]) % M;
-    cout << dap;
+    for(int i = 0; i <= 9; ++i) dap += dp[N][i];
+    cout << dap % M;
     return 0;
 }
