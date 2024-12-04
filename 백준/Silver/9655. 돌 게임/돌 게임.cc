d0c5b1ab462c7aa5d72ce61e78aadad53d3419f1
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2024-12-04 18:30:33 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -5,10 +5,8 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    int N, dp[1010] = {}; cin >> N;
-    dp[1] = SK; dp[2] = CY; dp[3] = SK;
-    for(int i = 4; i <= N; ++i) dp[i] = !dp[i - 1];
-    if(dp[N] == SK) cout << "SK";
+    int N; cin >> N;
+    if(N % 2) cout << "SK";
     else cout << "CY";
     return 0;
 }
