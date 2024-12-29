02ef51170c7e71187021a8494d64225d0a2ab1e1
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2024-12-30 05:33:34 +0900

    [Platinum IV] Title: 축사 배정, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Platinum/2188. 축사 배정/축사 배정.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> G[202];
+int C[202], A[202], dap, visitNum = 1;
+
+int dfs(int v){
+    for(auto i : G[v]){
+        if(C[i] == visitNum) continue;
+        C[i] = visitNum;
+        if(A[i] == 0 || dfs(A[i])){
+            A[i] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int N, M; cin >> N >> M;
+    for(int i = 1, n, v; i <= N; ++i){
+        cin >> n;
+        while(n--){
+            cin >> v;
+            G[i].emplace_back(v);
+        }
+    }
+    for(int i = 1; i <= N; ++i){
+        dap += dfs(i);
+        ++visitNum;
+    }
+    cout << dap;
+    return 0;
+}
