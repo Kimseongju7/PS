64146ac810b979276e1e3c2fb3020d1202fc0e76
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2025-01-20 20:01:13 +0900

    [Platinum V] Title: 연세워터파크, Time: 16 ms, Memory: 5200 KB -BaekjoonHub

---
+++ 백준/Platinum/15678. 연세워터파크/연세워터파크.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<deque>
+#include<utility>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    long long N, D, arr[101010], dp[101010];
+    cin >> N >> D; for(int i = 1; i <= N; ++i) cin >> arr[i];
+    deque<pair<long long, long long> > q;
+    dp[1] = arr[1];
+    for(int i = 1; i <= N; ++i){
+        if(!q.empty() && i - q.front().second > D) q.pop_front();
+        if(!q.empty())dp[i] = max(arr[i], q.front().first + arr[i]);
+        while(!q.empty() && q.back().first <= dp[i]) q.pop_back();
+        q.emplace_back(dp[i], i);
+    }
+    long long dap = -1e18;
+    for(int i = 1; i <= N; ++i) dap = max(dap, dp[i]);
+    dp[N] = arr[N];
+    q.clear();
+    for(int i = N; i > 0; --i){
+        if(!q.empty() && q.front().second - i > D) q.pop_front();
+        if(!q.empty())dp[i] = max(arr[i], q.front().first + arr[i]);
+        while(!q.empty() && q.back().first <= dp[i]) q.pop_back();
+        q.emplace_back(dp[i], i);
+    }
+    for(int i = 1; i <= N; ++i) dap = max(dap, dp[i]);
+    cout << dap;
+    return 0;
+}
