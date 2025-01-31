63856bbe62ec60c77bbfdc2146588170ff978696
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2025-01-31 23:43:20 +0900

    [Silver IV] Title: 사탕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1812. 사탕/사탕.cc
+++ 백준/Silver/1812. 사탕/사탕.cc
@@ -3,17 +3,17 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    int N, A[1010], dap[1010], t = 0; cin >> N;
+    int N, A[1010], t = 0; cin >> N;
     for(int i = 1; i <= N; ++i) {
         cin >> A[i];
         if(i & 1) t += A[i];
         else t -= A[i];
     }
-    dap[1] = t / 2;
-    cout << dap[1] << endl;
+    t /= 2;
+    cout << t << '\n';
     for(int i = 2; i <= N; ++i){
-        dap[i] = A[i - 1] - dap[i - 1];
-        cout << dap[i] << endl;
+        t = A[i - 1] - t;
+        cout << t << '\n';
     }
 
 
