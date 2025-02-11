1e33eb7f6bb2de96c3738e7e6166b5fc6c4ede76
Kimseongju7 <123197704+Kimseongju7@users.noreply.github.com>
2025-02-11 21:17:45 +0900

    [Silver IV] Title: 반음, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2034. 반음/반음.cc
+++ 백준/Silver/2034. 반음/반음.cc
@@ -5,18 +5,18 @@ int main(){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
     int N; int st, end;
     int arr[10101];
-    char P[15] = {'A', 'X', 'B', 'C', 'X', 'D', 'X', 'E', 'F', 'X', 'G', 'X'};
+    char P[15] = {'A', 0, 'B', 'C', 0, 'D', 0, 'E', 'F', 0, 'G', 0};
     int size = 12;
     cin >> N;
     for(int i = 1; i <= N; ++i) cin >> arr[i];
     for(int i = 0; i < size; ++i){
         st = i; end = i;
-        if(P[st] == 'X') continue;
+        if(P[st] == 0) continue;
         for(int j = 1; j <= N; ++j){
             end = end + arr[j];
-            if(end < 0) end += size;
             end %= size;
-            if(P[end] == 'X') break;
+            if(end < 0) end += size;
+            if(P[end] == 0) break;
             if(j == N) cout << P[st] << ' ' << P[end] << '\n';
         }
     }
