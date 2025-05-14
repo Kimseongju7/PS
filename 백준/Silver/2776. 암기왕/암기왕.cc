b9c7b6bdc4551055b2c84310f85540133c999593
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2025-05-14 14:42:01 +0900

    [Silver IV] Title: 암기왕, Time: 1784 ms, Memory: 49016 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int T, N, t;
+    cin >> T;
+    set<int> A;
+    T++;
+    while(--T){
+        cin >> N;
+        for(int i = 0; i < N; i++){
+            cin >> t;
+            A.insert(t);
+        }
+        cin >> N;
+        for(int i = 0; i < N; i++){
+            cin >> t;
+            if(A.count(t)) cout << 1;
+            else cout << 0;
+            cout << '\n';
+            
+        }
+        A.clear();
+    }
+    return 0;
+}
