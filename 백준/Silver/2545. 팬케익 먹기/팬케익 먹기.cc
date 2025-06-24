f4921b6bde70188067f903e3e1907cf009f5e11b
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2025-06-24 18:54:52 +0900

    [Silver IV] Title: 팬케익 먹기, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cc
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cc
@@ -1,30 +1,73 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    long long T, a, b, c, d, tmp;
+    unsigned long long T, a, b, c, d, tmp;
     cin >> T;
 
     while(T--){
         cin >> a >> b >> c >> d;
-        while(d > 0){
-            if(a >= b && a >= c){
-                tmp = min(d, a - max(b, c) + 1);
-                a -= tmp;
-                d -= tmp;
-            }
-            else if(b >= a && b >= c){
-                tmp = min(d, b - max(a, c) + 1);
-                b -= tmp;
-                d -= tmp;
-            }
-            else{
-                tmp = min(d, c - max(b, a) + 1);
-                c -= tmp;
-                d -= tmp;
-            }
+        if(a >= b && a >= c){
+            tmp = min(d, a - max(b, c));
+            a -= tmp;
+            d -= tmp;
         }
+        else if(b >= a && b >= c){
+            tmp = min(d, b - max(a, c));
+            b -= tmp;
+            d -= tmp;
+        }
+        else{
+            tmp = min(d, c - max(b, a));
+            c -= tmp;
+            d -= tmp;
+        }
+        // cout << "1 : " << a << ' ' << b << ' ' << c << ' ' << d << endl;
+        if(d == 0) {
+            cout << a * b * c << '\n';
+            
+            continue;
+        }
+        
+        if(a <= b && a <= c){
+            tmp = min(d, (b - a) * 2);
+            d -= tmp;
+            b -= tmp % 2 == 0 ? tmp / 2ULL : tmp / 2ULL + 1ULL;
+            c -= tmp/2ULL;
+        }
+        else if(b <= a && b <= c){
+            tmp = min(d, (a - b) * 2);
+            d -= tmp;
+            a -= tmp % 2 == 0 ? tmp / 2ULL : tmp / 2ULL + 1ULL;
+            c -= tmp/2ULL;
+        }
+        else{
+            tmp = min(d, (b - c) * 2);
+            d -= tmp;
+            b -= tmp % 2 == 0 ? tmp / 2ULL : tmp / 2ULL + 1ULL;
+            a -= tmp/2ULL;
+        }
+        // cout << "2 : " << a << ' ' << b << ' ' << c << ' ' << d << endl;
+        if(d == 0) {
+            cout << a * b * c << '\n';
+            continue;;
+        }
+
+        tmp = d / 3ULL;
+        a -= tmp;
+        b -= tmp;
+        c -= tmp;
+        if(d % 3 == 2){
+            a--;
+            b--;
+        }
+        else if(d % 3 == 1){
+            a--;
+        }
+        
+        // cout << "3 : " << a << ' ' << b << ' ' << c << ' ' << d << endl;
         cout << a * b * c << '\n';
     }
     return 0;
