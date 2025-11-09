07991b07f51e500962ed077d14ddadb608598e1d
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2025-11-09 23:28:14 +0900

    [Bronze IV] Title: Zero or One, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13623. Zero or One/Zero or One.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b, c; cin >> a >> b >> c;
+    if(a != b && a != c) cout << 'A';
+    else if(b != a && b != c) cout << 'B';
+    else if(c != a && c != b) cout << 'C';
+    else cout << '*';
+    return 0;
+}
