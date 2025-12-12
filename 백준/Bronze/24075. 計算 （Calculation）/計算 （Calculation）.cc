174506322999260211ba3b2d9c57e7bd558378b0
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2025-12-12 21:02:04 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b; cin >> a >> b;
+    cout << max(a + b, a - b) << '\n';
+    cout << min(a + b, a - b);
+    return 0;
+}
