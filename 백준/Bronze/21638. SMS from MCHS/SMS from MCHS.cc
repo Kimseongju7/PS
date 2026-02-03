c5fcc1cf6a32a44aeb5219817070e76997349dc3
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2026-02-03 15:52:28 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    if(c < 0 && d >= 10) cout << "A storm warning for tomorrow! Be careful and stay home if possible!";
+    else if(a > c) cout << "MCHS warns! Low temperature is expected tomorrow.";
+    else if(b < d) cout << "MCHS warns! Strong wind is expected tomorrow.";
+    else cout << "No message";
+    return 0;   
+}
