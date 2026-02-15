c70e04dba292587e1bad40f87e7a61cee11762d8
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2026-02-16 03:27:40 +0900

    [level 3] Title: 정수 삼각형, Time: 0.33 ms, Memory: 7.73 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -4,31 +4,11 @@
 
 using namespace std;
 
-int solution(vector<vector<int>> triangle) {
-    int D[1010][1010];
-    int R = triangle.size();
-    int C = triangle[R - 1].size();
-    D[0][0] = triangle[0][0]; 
-    // for(int i = 0; i < R; i++){
-    //     for(int j = 0; j <= i; j++) cout << triangle[i][j] << ' ';
-    //     cout << endl;
-    // }
-    // cout << "--------------" << endl;
-    for(int i = 1; i < R; i++){
+int solution(vector<vector<int>> t) {
+    for(int i = t.size() - 2; i >= 0; i--){
         for(int j = 0; j <= i; j++){
-            for(int k = j - 1; k <= j; k++){
-                if(k < 0 || k >= i) continue;
-                D[i][j] = max(D[i][j], triangle[i][j] + D[i - 1][k]);
-            }
+            t[i][j] += max(t[i + 1][j], t[i + 1][j + 1]);
         }
     }
-    int dap = 0; 
-    for(int i = 0; i < C; i++) dap = max(dap, D[R - 1][i]);
-    
-    // for(int i = 0; i < R; i++){
-    //     for(int j = 0; j <= i; j++) cout << D[i][j] << ' ';
-    //     cout << endl;
-    // }
-    // cout << "--------------" << endl;
-    return dap;
+    return t[0][0];
 }
