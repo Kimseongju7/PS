64a65b3ebfa5148b12d9e28685a7b65993df9a25
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2026-02-24 22:17:20 +0900

    [Bronze I] Title: snails, Time: 104 ms, Memory: 4068 KB -BaekjoonHub

---
+++ 백준/Bronze/34434. snails/snails.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+long long A[2 << 17] = {};
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int T, N; 
+    string dap;
+    cin >> T; cin.ignore(); 
+    while(T--){
+        N = 0; long long t;
+        dap = "NAUTILUS";
+        while(cin.peek() != '\n'){
+            cin >> t; 
+            A[N++] = t;
+        }
+        cin.ignore();
+
+        for(int i = 0; i < N; i++){
+            if(i < 4){
+                if(i != A[i]) {
+                    dap = "SNAIL";
+                    break;
+                }
+            }
+            else if(A[i] != (A[i - 1] + A[i - 2] + A[i - 3] + A[i - 4])){
+                dap = "SNAIL";
+                break;
+            }
+        }
+        cout << dap << '\n';
+    }
+    return 0;
+}
